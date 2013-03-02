1095166954d7994465dc7a8d342743268819f000
filipmedia <filipmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-02 03:02:41 +0000

    Tagging new version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/fb-comments-importer/tags/1.0.1@675248 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== FB Comments Importer ===
+Contributors: filipmedia
+Donate link: 
+Tags: fb comments, facebook, comments, fb comments import, facebook comments, facebook comments import, discussion
+Requires at least: 3.3
+Tested up to: 3.5.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Import Facebook comments into Wordpress.
+
+== Description ==
+
+FB Comments Importer is a simple plugin that lets you import comments from your Facebook page directly into Wordpress comments system.
+It gives your site an SEO boost and it makes it look even more alive.
+
+Since this is the first version, there are possibly some bugs so please open a support ticket if you notice one and we will fix it ASAP!
+
+== Installation ==
+
+1. Unzip `FBCommentsImporterFree.zip` to your `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Setup the plugin options by using the `FB Comments Importer` page
+
+== Frequently asked questions =
+
+== Screenshots ==
+
+== Changelog ==
+
+- 1.0 - 
+The initial release
+- 1.0.1-
+Fixed bug with manual import giving fatal error on some server configs
+
+== Upgrade notice ==
---
+++ update_form.php
@@ -0,0 +1,51 @@
+<table width="100%">
+    <tr>
+        <td width="50%">
+            <form action ="?page=fbsync_comments_free&action=save_data" method="POST">
+                <table>
+                    <tr>
+                        <td>Facebook Fan Page ID :</td>
+                        <td><input name="pageID" type="text" value="<?= $pageID; ?>" class="regular-text"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://developers.facebook.com/apps" target="_blank">APP ID:</a></td>
+                        <td><input name="appID" type="text" value="<?= $appID; ?>" class="regular-text"></td>
+                    </tr>
+                    <tr>
+                        <td><a href="https://developers.facebook.com/apps" target="_blank">APP Secret Code:</a></td>
+                        <td><input name="appSecret" type="text" value="<?= $appSecret; ?>" class="regular-text"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" name="submit" value="Save"></td>
+                    </tr>
+                </table>
+            </form>
+        </td>
+        <td valign="top">
+            <div style="border: 1px solid #000000; padding: 5px; background: #DBFFE9; height: 200px; border-radius: 15px;">
+                <center>
+                <h2><a href="http://projects.geekydump.com" target="_blank">Get Premium version now!</a></h2>
+                </center>
+                <table width="100%">
+                    <tr>
+                        <td>
+                            <b>Premium Features: </b><br>
+                            <ul>
+                                <li>- Import comments from images</li>
+                                <li>- Show unlimited posts</li>
+                                <li>- Manually connect post with comments</li>
+                                <li>- Import comments automatically (with WP cron)</li>
+                                <li>... and more...</li>
+                            </ul>
+                        </td>
+                        <td align="center" valign="top">
+                            <h1>Only $24.99</h1>
+                            <h3><a href="http://projects.geekydump.com" target="_blank">Order now!</a></h3>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+    </tr>
+</table>
